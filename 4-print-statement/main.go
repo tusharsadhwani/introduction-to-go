378ed90b0653291656b51856a64470f5a5a2c67f
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-01-13 20:49:14 +0530

    added documentation

--- 4-print-statement/main.go
+++ 4-print-statement/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var isGolangBoring bool
+	var isGolangBoring bool // all variables have a default "zero value"
 	magicNumber := 42
 	fullName := "Tushar Sadhwani"
 
@@ -15,5 +15,5 @@ func main() {
 	fmt.Printf("My name is %v, ", fullName)
 	fmt.Printf("this is a %T\n", fullName)
 
-	fmt.Print("->")
+	fmt.Print("->") // Print doesn't add a newline by default
 }
