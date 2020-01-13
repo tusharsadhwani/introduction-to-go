18994accefdc81166130d20cd65fa1442019033e
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-01-13 21:53:53 +0530

    added slide 5

--- 5-arrays-and-slices/main.go
+++ 5-arrays-and-slices/main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var isGolangBoring bool
-	magicNumber := 42
-	fullName := "Tushar Sadhwani"
+	arr1 := [3]int{5, 6, 7}   // Array of length 3
+	arr2 := [...]int{5, 6, 7} // Alternative syntax
 
-	fmt.Println(isGolangBoring)
+	arr1[0] = 500
+	fmt.Println("arr1:", arr1)
+	fmt.Println("arr2:", arr2)
 
-	fmt.Printf("%v\n", magicNumber)
-	fmt.Printf("%T\n", magicNumber)
-
-	fmt.Printf("My name is %v, ", fullName)
-	fmt.Printf("this is a %T\n", fullName)
-
-	fmt.Print("->")
+	slice1 := []int{1, 2, 3, 4}    // Slices don't have fixed length
+	slice2 := append(slice1, 5, 6) // Doesn't affect the original slice
+	fmt.Println("slice1:", slice1)
+	fmt.Println("slice2:", slice2)
 }
