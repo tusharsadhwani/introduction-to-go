032614e0cf4eda147a688928473bfe2da592b059
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-01-14 18:54:32 +0530

    added part 7

---
+++ 7-functions/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func avg(a, b int) float64 {
+	ans := float64(a+b) / 2
+	return ans
+}
+
+func main() {
+	val1 := 5
+	val2 := 7
+	avgVal := avg(val1, val2)
+	fmt.Println(avgVal)
+	fmt.Println(avg(32, 52))
+
+	greet := func() {
+		fmt.Println("Hello Techies!")
+	} // Inline Function
+	greet()
+}
